191178867e93fcffbd272672c68cb7e2e681eb29
tomsmoker <tomsmoker@gmail.com>
2024-04-25 22:13:20 -0700

    v0.1.4

--- src/whyhow_rbr/__init__.py
+++ src/whyhow_rbr/__init__.py
@@ -6,7 +6,7 @@ from whyhow_rbr.exceptions import (
 )
 from whyhow_rbr.rag import Client, Rule
 
-__version__ = "v0.1.3"
+__version__ = "v0.1.4"
 __all__ = [
     "Client",
     "IndexAlreadyExistsException",
